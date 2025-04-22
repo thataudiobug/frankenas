4851deb63e1c80a82fad454253097bcd39aa44cf
thataudiobug <github@thataudiobug.com>
2025-04-22 05:44:20 -0400

    Create port-tortuga-provision.sh

---
+++ port-tortuga-provision.sh
@@ -0,0 +1,21 @@
+# get updates and update to 24 LTS #
+apt-get update && apt-get upgrade -y
+sed -i 's/jammy/noble/g' /etc/apt/sources.list
+apt-get update && apt-get upgrade -y
+# Add Docker's official GPG key: #
+apt-get update
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+# Install docker-ce
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# install portainer
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
