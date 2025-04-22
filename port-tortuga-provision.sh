7a13085b2f49672b29b2bb18a6bfb86b0fea1228
thataudiobug <github@thataudiobug.com>
2025-04-22 16:57:08 -0400

    Update port-tortuga-provision.sh

--- port-tortuga-provision.sh
+++ port-tortuga-provision.sh
@@ -16,6 +16,32 @@ echo \
 apt-get update
 # Install docker-ce
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Instal Qbittorrent
+docker run -d \
+  --name=qbittorrent \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Etc/UTC \
+  -e WEBUI_PORT=8080 \
+  -e TORRENTING_PORT=6881 \
+  -p 8080:8080 \
+  -p 6881:6881 \
+  -p 6881:6881/udp \
+  -v /path/to/qbittorrent/appdata:/config \
+  -v /path/to/downloads:/downloads `#optional` \
+  --restart unless-stopped \
+  lscr.io/linuxserver/qbittorrent:latest
+  # Install Prowlarr
+  docker run -d \
+  --name=prowlarr \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Etc/UTC \
+  -p 9696:9696 \
+  -v /path/to/prowlarr/data:/config \
+  --restart unless-stopped \
+  lscr.io/linuxserver/prowlarr:latest
+  
 # install portainer
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
