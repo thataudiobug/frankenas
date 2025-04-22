41b73a160ce4c7af77178441bf15a715084fb3b6
thataudiobug <github@thataudiobug.com>
2025-04-22 16:56:25 -0400

    Create port-royal-provision.sh

---
+++ port-royal-provision.sh
@@ -0,0 +1,39 @@
+# get updates and update to 24 LTS #
+apt-get update && apt-get upgrade -y
+sed -i 's/jammy/noble/g' /etc/apt/sources.list
+apt-get update && apt-get upgrade -y
+# Add Docker's official GPG key: #
+apt-get update
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+# Install docker-ce
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Install NPM
+docker run -d \
+  --name=npm \
+  -p 80:80 \
+  -p 81:81 \
+  -p 443:443 \
+  -v /npm/data:/data \
+  -v /npm/letsencrypt:/letsencrypt \
+  --restart unless-stopped \
+  jc21/nginx-proxy-manager:latest
+# Install Ombi
+ docker run -d \
+  --name=ombi \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Etc/UTC \
+  -e BASE_URL=/ `#optional` \
+  -p 3579:3579 \
+  -v /path/to/ombi/config:/config \
+  --restart unless-stopped \
+  lscr.io/linuxserver/ombi:latest
