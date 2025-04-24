64995ecb6ea65cd98d03940eca966e0ee2974444
thataudiobug <github@thataudiobug.com>
2025-04-23 20:56:53 -0400

    Update port-tortuga-provision.sh

--- port-tortuga-provision.sh
+++ port-tortuga-provision.sh
@@ -2,46 +2,12 @@
 apt-get update && apt-get upgrade -y
 sed -i 's/jammy/noble/g' /etc/apt/sources.list
 apt-get update && apt-get upgrade -y
-# Add Docker's official GPG key: #
-apt-get update
-apt-get install -y ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-# Install docker-ce
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# Instal Qbittorrent
-docker run -d \
-  --name=qbittorrent \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e TZ=Etc/UTC \
-  -e WEBUI_PORT=8080 \
-  -e TORRENTING_PORT=6881 \
-  -p 8080:8080 \
-  -p 6881:6881 \
-  -p 6881:6881/udp \
-  -v /path/to/qbittorrent/appdata:/config \
-  -v /path/to/downloads:/downloads `#optional` \
-  --restart unless-stopped \
-  lscr.io/linuxserver/qbittorrent:latest
-  # Install Prowlarr
-  docker run -d \
-  --name=prowlarr \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e TZ=Etc/UTC \
-  -p 9696:9696 \
-  -v /path/to/prowlarr/data:/config \
-  --restart unless-stopped \
-  lscr.io/linuxserver/prowlarr:latest
-  
-# install portainer
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
+# Install dependancies.
+apt install -y dirmngr ca-certificates software-properties-common apt-transport-https
+# install Qbit repo and update package lists
+add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
+apt update
+# Install Qbit
+apt install qbittorrent-nox
+# Add Start Service
+systemctl start qbittorrent-nox
