0d155f27d03022e3e28e960d16eed4c5dda1ddf3
thataudiobug <github@thataudiobug.com>
2025-05-29 15:59:21 -0400

    Update jellyfin-create.sh

--- jellyfin-create.sh
+++ jellyfin-create.sh
@@ -1,5 +1,5 @@
 mp0: /essek/media/,mp=/content
-mp1: /essek/media/meta,mp=/meta
+mp1: caleb:subvol-101-disk-1,mp=/transcodes,backup=1,size=32G
 lxc.cgroup2.devices.allow: c 226:128 rwm
 lxc.mount.entry: /dev/dri/renderD128 dev/dri/renderD128 none bind,optional,create=fi>
 lxc.hook.pre-start: sh -c "chown 100000:100108 /dev/dri/renderD128"
