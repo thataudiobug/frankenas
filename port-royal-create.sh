0b6aefef328222ca16cd736b855135d1297038db
thataudiobug <github@thataudiobug.com>
2026-03-19 10:49:13 -0400

    Update port-royal-create.sh

--- port-royal-create.sh
+++ port-royal-create.sh
@@ -1,6 +1,69 @@
-mp0: /essek/media,mp=/mnt/media
-mp1: /Caleb/docker/configs,mp=/mnt/config
-mp2: /essek,mp=/mnt/essek
-mp3: /Nott,mp=/mnt/nott
-mp4: /Yasha,mp=/mnt/yasha
-mp5: /Caleb,mp=/mnt/caleb
+#!/bin/bash
+echo "Welcome to Casey's Port Royal LXC spinnup script."
+
+# Default vars
+var_name="port-royal"
+var_cpus="2"
+var_mem="4096"
+var_swap="512"
+var_fsvol="caleb"
+var_fsgb="8"
+var_wan_bridge="vmbr1"
+var_inet_bridge="vmbr0"
+var_gwid="254"
+var_dns_local="254"
+
+# Get the ct ID
+read -p 'Please enter this containers ID: ' var_cid
+
+# Function for configuring network info
+config_network () {
+  read -p 'Please enter the Wan Bridge ID (vmbr1): ' var_wan_bridge
+  read -p 'Please enter the Inet Bridge ID (vmbr0): ' var_inet_bridge
+  read -p 'Please enter the gateway ID: ' var_gwid
+  read -p 'Please enter the local DNS ID: ' var_dns_local
+}
+
+# Function for configuing everything else
+config_all_else () {
+  read -p 'Please enter the CT name: ' var_name
+  read -p 'Please enter the CPU count: ' var_cpus
+  read -p 'Please enter the memory in MBs: ' var_mem
+  read -p 'Please enter the swap in MBs: ' var_swap
+  read -p 'Please enter the storage where the root file system should live: ' var_fsvol
+  read -p 'Please enter the root file system size in GBs: ' var_fsgb
+  config_network
+}
+
+# Fucntion to check if shit has changed.
+echo 'Do any settings need manual input?'
+select var_install_type in "None" "Networking" "All" 
+do
+    case $var_install_type in
+        None) break;;
+        Networking) 
+          config_network break;;
+        All)
+          config_all break;;
+    esac
+done
+
+pct create $var_cid local:vztmpl/ubuntu-24.04-standard_24.04-1_amd64.tar.zst \
+  --hostname $var_name \
+  --cores $var_cpus \
+  --memory $var_mem \
+  --swap $var_swap \
+  --rootfs $var_fsvol:$var_fsgb \
+  --net0 name=eth0,bridge=$var_wan_bridge,ip=192.168.1.$var_cid/24,gw=192.168.1.$var_gwid \
+  --net1 name=eth1,bridge=$var_inet_bridge,ip=192.168.8.$var_cid/24 \
+  --nameserver 192.168.1.$var_dns_local,1.1.1.1,8.8.8.8 \
+  --onboot 1 \
+  --features nesting=1 \
+  --unprivileged 1 \
+  --ssh-public-keys <(curl -fsSL https://github.com/thataudiobug.keys) \
+  --mp0 /essek/media,mp=/mnt/media \
+  --mp1 /Caleb/docker/configs,mp=/mnt/config \
+  --mp2 /essek,mp=/mnt/essek \
+  --mp3 /Nott,mp=/mnt/nott \
+  --mp4 /Yasha,mp=/mnt/yasha \
+  --mp5 /Caleb,mp=/mnt/caleb \
