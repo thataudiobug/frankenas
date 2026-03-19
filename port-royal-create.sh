1396f8e7a3a729c1650cf56e3b5f6335d1df8d14
thataudiobug <github@thataudiobug.com>
2026-03-19 08:42:17 -0400

    Update port-royal-create.sh

--- port-royal-create.sh
+++ port-royal-create.sh
@@ -1,1 +1,6 @@
-mp0: /Caleb/docker/,mp=/config/
+mp0: /essek/media,mp=/mnt/media
+mp1: /Caleb/docker/configs,mp=/mnt/config
+mp2: /essek,mp=/mnt/essek
+mp3: /Nott,mp=/mnt/nott
+mp4: /Yasha,mp=/mnt/yasha
+mp5: /Caleb,mp=/mnt/caleb
