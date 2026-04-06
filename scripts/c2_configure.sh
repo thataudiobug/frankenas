54e15d761e44390081dd031b077c0dedda1d75ba
thataudiobug <github@thataudiobug.com>
2026-04-06 09:12:22 -0400

    Update c2_configure.sh

--- scripts/c2_configure.sh
+++ scripts/c2_configure.sh
@@ -5,5 +5,14 @@ apt update && apt upgrade -y
 echo "Installing git, curl, and ansible"
 apt install git curl ansible -y
 
-echo "cloneing repo"
+echo "Cloneing repo"
 git clone git@github.com:thataudiobug/frankenas.git
+
+echo "Requesting secrets"
+VAULTKEYFILE="$home/vault/keyfile"
+read -ps 'Please paste the vault key: ' SSHKEY
+rm "$VAULTKEYFILE"
+touch "$VAULTKEYFILE"
+echo "$SSHKEY" >> "$VAULTKEYFILE" 
+echo
+echo "Secrets setup... Done"
